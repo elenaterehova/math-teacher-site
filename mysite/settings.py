a53dbf7193e28d2775795f1e5a92ff5a611b1b6d
Пользователь <tterekhovaa@mail.ru>
2023-07-30 19:28:37 +0300

    изменить debug на false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bt)vayne7i@^dls8kev4^rug!#0u2!na(%hr7o3uc&i3zl0x24'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
