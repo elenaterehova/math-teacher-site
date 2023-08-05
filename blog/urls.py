5f7f5d17c6d5a9f0d85b0f6063521febd1a3de5b
Пользователь <tterekhovaa@mail.ru>
2023-08-06 02:23:04 +0300

    изменить шаблон и отображение записей из бд

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('', views.index, name='index'),
+    path('post/<int:pk>/', views.post_number, name='post_number'),
+    path('mine/', views.MyView.as_view(), name='my-view'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
+from django.views import View
+
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 from .models import SocialMedia
+from django.http import HttpResponse
 
+
+class MyView(View):
+
+    def get(self, request, pk):
+        return HttpResponse('Hello, World!')
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_number(request, my_pk):
+    post = get_object_or_404(Post,pk=my_pk)
+    return render(request, 'blog/post_list.html', {'post': post})
 def post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -43,5 +54,6 @@ def post_edit(request, pk):
 
 def socialmedia_link(request, pk):
     link = get_object_or_404(SocialMedia, pk)
-    return redirect('http://telegram.org', pk=link.pk)
-
+    return redirect(request, 'blog/base.html', pk=link.pk)
+def index(request):
+    return HttpResponse("Страница приложения")
